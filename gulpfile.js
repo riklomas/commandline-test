d3b567f1c52671e7adcb1f704062c607edd3431b
Alex Sweet <alexsweet31@gmail.com>
2019-01-12 19:07:00 -0500

    added gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+sass.compiler = require('node-sass');
+
+
+gulp.task('default', function(){
+    return gulp.src('css/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app.css'))
+})
+
